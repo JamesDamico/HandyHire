d49f29a23e53abcce13b024f69453d31d59e0f90
James D'Amico <JamesTDamico@gmail.com>
2020-10-13 18:29:04 -0400

    Renamed file paths

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const port = 3000;
 //Public CSS files
 app.use(express.static("public"));
 
+//Home route
 app.get("/", (req, res)=>{
     res.sendFile(__dirname + "/HTML/homePage.html");
 });
 
+
+//Establishing connection to the server
 app.listen(process.env.PORT || port, ()=>{
     console.log("Server running on port: " + port);
 });
