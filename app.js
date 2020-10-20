1823581ae833d7390b25bdd45ac77468f30fc29d
James D'Amico <JamesTDamico@gmail.com>
2020-10-19 21:02:08 -0400

    Implemented EJS
    Converted backend to use EJS

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
+//Express
 const express = require("express");
 const app = express();
 const port = 3000;
+
+//EJS
+const ejs = require("ejs");
+app.set('view engine', 'ejs');
 
 //Public CSS files
 app.use(express.static("public"));
 
 //Home route
 app.get("/", (req, res)=>{
-    res.sendFile(__dirname + "/HTML/homePage.html");
+    res.render("homePage.ejs");
 });
 
+//Edit Profile route
+app.get("/editProfile", (req, res)=>{
+    res.render("homePage.ejs");
+});
+
+//Browse route
+app.get("/browse", (req, res)=>{
+    res.render("browse.ejs");
+});
 
 //Establishing connection to the server
 app.listen(process.env.PORT || port, ()=>{
     console.log("Server running on port: " + port);
 });
+
